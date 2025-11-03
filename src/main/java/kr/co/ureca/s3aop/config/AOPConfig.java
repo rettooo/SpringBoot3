5f8d590bee079d246186e2889aa4f67130599595
rettooo <gkdud9236@naver.com>
2025-11-03 16:37:49 +0900

    s3 aop: AOP - AOP 의존성 추가 (@Aspect) - aopBefore() 메소드 추가 대상 메서드가 실행되기 전 실행할 수 있다.

---
+++ src/main/java/kr/co/ureca/s3aop/config/AOPConfig.java
@@ -0,0 +1,28 @@
+package kr.co.ureca.s3aop.config;
+
+import kr.co.ureca.s1di.controller.DIController;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Aspect
+@Component //bean에 등록
+public class AOPConfig {
+    public static final Logger logger = LoggerFactory.getLogger(DIController.class);
+
+    //모든 return 타입/ kr.co.ureca.모든 클래스.controller -> class, method, argument
+    @Before("execution(* kr.co.ureca.*.controller.*.*(..))")
+    public void aopBefore(JoinPoint joinPoint){
+        logger.info(">>> AOP before start <<<");
+        logger.info(joinPoint.toString()); //어떤 정보?
+        logger.info("AOP Before time: " + new Date().toString()); //어떤 정보?
+
+        logger.info(">>> AOP before end <<<");
+
+    }//aopBefore
+}
