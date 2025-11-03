2fa4ee09dee802586c18378c23efc360f518ff35
rettooo <gkdud9236@naver.com>
2025-11-03 15:12:37 +0900

    DIService class 생성 - Service Map 출력 테스트 - 변수 선언: slf4j logger -> sout 대신 생성 - logger 출력될때 class 이름도 나옴 (DIController도 logger 추가)

--- src/main/java/kr/co/ureca/s1di/controller/DIController.java
+++ src/main/java/kr/co/ureca/s1di/controller/DIController.java
@@ -1,3 +1,19 @@
 package kr.co.ureca.s1di.controller;
+
+import kr.co.ureca.s1di.service.DIService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+//@Configuration
+//@Component //bean 주입
+//@Controller // result mapping
+
+@Controller
 public class DIController {
+    public static final Logger logger = LoggerFactory.getLogger(DIController.class);
+
+
 }
--- src/main/java/kr/co/ureca/s1di/service/DIService.java
+++ src/main/java/kr/co/ureca/s1di/service/DIService.java
@@ -1,4 +1,21 @@
 package kr.co.ureca.s1di.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class DIService {
+    public static final Logger logger = LoggerFactory.getLogger(DIService.class);
+
+    public Map<String, String> diTest(){
+        logger.info(">>> di test<<<");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("isSuccess", "true");
+        map.put("message", "this is di test..");
+        return map;
+    }
 }
