38cf0cf5bf3f73da637821c917195892b1afac08
rettooo <gkdud9236@naver.com>
2025-11-03 15:33:12 +0900

    DIController 의존성 주입 - 3. Setter method에 의한 자동 의존성 주입 - @Autowired 없으면 에러 - 쓰지 않는 이유

--- src/main/java/kr/co/ureca/s1di/controller/DIController.java
+++ src/main/java/kr/co/ureca/s1di/controller/DIController.java
@@ -3,6 +3,7 @@ package kr.co.ureca.s1di.controller;
 import kr.co.ureca.s1di.service.DIService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -27,18 +28,29 @@ public class DIController {
     // 생성자가 실행될때 ->DiService가 필요함. => Spring이 생성하고 제공한다.
     // 공식적으로 권장되는 방식이다.
     // why? 의존객체(DIService)가 없으면, DIController 생성 불가
-    private DIService service;
+//    private DIService service;
+//
+//    public DIController(DIService service) {
+//        this.service = service;
+//    }
 
-    public DIController(DIService service) {
-        this.service = service;
-    }
+    //3.  Setter method에 의한 자동 의존성 주입
+//    private DIService service;
+//    @Autowired //현재 객체(DIController)가 실행될 때, Service가 자동으로 실행 및 제공
+//    public void setService(DIService service){
+//        this.service = service;
+//    }
+
+    //4.
+
 
 
     @GetMapping(value="/ditest")
     @ResponseBody //response가 html body 부분임
     public Map<String, String> diTest(){ //객체 리턴
 //        logger.info("1. 17 line 개발자가 DIService 객체 주입 ");
-        logger.info("2.생성자에 의한 자동 의존성 주입 ");
+//        logger.info("2.생성자에 의한 자동 의존성 주입 ");
+        logger.info("3. setter method에 의한 자동 의존성 주입 ");
         Map<String, String> map = service.diTest();
         return map;
     }
